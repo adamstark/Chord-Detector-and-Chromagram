f1c16a2ffc9d3f222f0348218b362bdbe8cfcb86
Adam Stark <adamstark.uk@gmail.com>
2014-06-14 16:54:14 +0100

    Adding the new re-written version of the algorithm

---
+++ unit-tests/Chord Tests/tests/Test_Signals.h
@@ -0,0 +1,16 @@
+//
+//  Test_Signals.h
+//  Chord Tests
+//
+//  Created by Adam Stark on 14/06/2014.
+//  Copyright (c) 2014 ___adamstark___. All rights reserved.
+//
+
+#ifndef __Chord_Tests__Test_Signals__
+#define __Chord_Tests__Test_Signals__
+
+extern double C_Major_44_1[50176];
+
+extern double C_Major_48[54613];
+
+#endif /* defined(__Chord_Tests__Test_Signals__) */
