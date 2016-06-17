ec1fad71dfbdc11efef8facde66e6205caae672c
Adam Stark <adamstark.uk@gmail.com>
2016-06-17 22:36:07 +0100

    Documentation update

---
+++ doc/html/search/searchdata.js
@@ -0,0 +1,33 @@
+var indexSectionsWithContent =
+{
+  0: "cdgipqrs~",
+  1: "c",
+  2: "c",
+  3: "cdgips~",
+  4: "iqr",
+  5: "c",
+  6: "c"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "enums",
+  6: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Enumerations",
+  6: "Pages"
+};
+
